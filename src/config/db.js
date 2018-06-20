761a53286122a3b500037b48e8a80884c34c7d81
was <zxt_zel_npl@163.com>
2018-06-20 09:35:41 +0800

    delete the untrack files

--- src/config/db.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  host: '106.14.21.199',
-  user: 'root',
-  password: '761227',
-  database: 'tuiguang',
-  port: 3306,
-  dateStrings: true // 时间以字符串形式显示，否则会有类似这样的显示：Thu Apr 14 2016 00:00:00 GMT+0800 (中国标准时间) 17:20:12
-}
